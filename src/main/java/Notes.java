8d49c3f3d63ef1504b93dc81feafdab1000cc9f6
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-26 19:46:06 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -17,83 +17,83 @@ class Notes {
 
 		ArrayList<Integer> arrL = new ArrayList<Integer>(); //create an empty arraylist
 
-		//Use methods to access ArrayLists, do not use Array notation
-
+		//uses methods to access ArrayList, you can't use array notation
 
 		//add(value)
-		arrL.add(3); //adds the value to the end of the arrayList
-
-		//get(index) - get you the value from the provided index
-		int num = arrL.get(0); //unbox for you into an int
-		System.out.println(num);
-		//clear() - remove all elements from the arrayList
-		arrL.clear();
-		// System.out.println(arrL.get(0));  //Index out of bounds since there is nothing in the arrayList
-
-		//size() - length does not exist as a way to get the length of the arraylist.  Instead we use .size() for arrayList
-
-		System.out.println(arrL.size());
-		
-		//set(index, value) - replaces the value the given index
 
 		arrL.add(3);
-		arrL.add(2);
-		arrL.set(0, 4);
-		System.out.println(arrL); //ArrayList can simply just be printed
-		// int[] arr = new int[]{1, 3 , 5};
-		// System.out.println(arr);
+
+		//get(index)
+
+		System.out.println(arrL.get(0));
+
+		//clear
+
+		arrL.clear();
+
+		//output an arrayList
+
+		System.out.println(arrL);
+
+		//size()
+
+		System.out.println(arrL.size());
+
+		//set(index, value)
+
+		arrL.add(5);
+		
+		System.out.println(arrL);
+		arrL.set(0, 3);
+
+		System.out.println(arrL);
 
 		//remove
 
 		int a = 0; //index
-		Integer b = 2; //value
-		
-		arrL.remove(b);
+		Integer b = 3; //value
+
+		arrL.add(2);
 		System.out.println(arrL);
 
-		//Createing a HashMap
+		arrL.remove(0); //defaults to int not Integer
+		// arrL.remove(b);
+		System.out.println(arrL);
+		
+		//HashMaps
 
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		//creates an empty HashMap
+		//empty hashmap
 
-		//first data type -> Key
-		//second data type -> Value (definition)
+		//first data type -> key
+		//second data type -> value
 
-		//Keys have to be unique
-		//Values do not have to unique
-		
-		//Adding to a HashMap
+		//keys must be unique 
+		//value do not need to be unique
+
+		//add to a hashmap
 
 		map.put("a", 1);
 		map.put("b", 2);
 		map.put("c", 3);
 		map.put("d", 1);
-		// map.put("a", 5); //KEYS NEED TO BE UNIQUE
-		
+
 		//Getting a Value from a HashMap -> access by key
 
 		System.out.println(map.get("c"));
 
-		//remove a key and value from a hashmap -> remove on the key
+		//remove a key
 
 		map.remove("d");
-		
-		//clearing a hashmap
-
-		// map.clear();
-
-		//size
-
-		System.out.println(map.size());
-
-		//print a hashmap
-
 		System.out.println(map);
 
-		//using hashmap vs parallel arrays/arraylists
+		//map.clear()
 
-		//HashMap -> When we have 2 groups data.  And we don't care about order, HashMaps are faster
-		//Parallel Array/Arraylists -> More than 2 groups of data, When Order Matters
-		
+		//size()
+
+		//hashmaps vs parallel arrays/arraylists
+
+		//HashMap -> Order does NOT matter, Have two associated value, Hashmaps are faster
+		//Parallel Array/ArrayList -> If you want repeated keys, order matters, more than 2 associated
 	}
 }
